082ec3400ea4739595ecf86a299df20ac3393db4
Vansh Goel <34906419+vanshg395@users.noreply.github.com>
2022-06-07 20:01:16 +0530

    fixed routes

--- lib/services/routing/routes.dart
+++ lib/services/routing/routes.dart
@@ -2,7 +2,4 @@ class Routes {
   Routes._();
 
   static const splash = '/splash';
-  static const home = '/';
-  static const mainScreen = '/main';
-  static const fitnessCentre = '/fitness-centre';
 }
