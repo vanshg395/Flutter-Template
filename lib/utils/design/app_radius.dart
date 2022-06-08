959fd2456ec02af3b4e4fd604d66d612d54ef48a
Vansh Goel <34906419+vanshg395@users.noreply.github.com>
2022-06-08 12:07:48 +0530

    radius modification

--- lib/utils/design/app_radius.dart
+++ lib/utils/design/app_radius.dart
@@ -5,5 +5,6 @@ class AppRadius {
   static const double xs = 4.0;
   static const double s = 6.0;
   static const double m = 12.0;
-  static const double l = 24.0;
+  static const double l = 16.0;
+  static const double xl = 24.0;
 }
